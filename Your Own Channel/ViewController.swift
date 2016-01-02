ded94115f7177eb4231ea82676e750a27b2bbd3f
kylepaul <kylegibbons@kyles-mbp.home>
2016-01-02 17:28:27 +0000

    forked version of own channel running super sport 1 channel

---
+++ Your Own Channel/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Your Own Channel
+//
+//  Created by Kane2931 on 2015-11-11.
+//  Copyright © 2015 Kane2931. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    // Paste your URL below between the " " to customize your channel (Default: NASA TV).
+    
+    
+    
+    
+    var player = AVPlayer(URL: NSURL(string: "http://live01.pushpublish.tv/mylive/livetv01/playlist.m3u8")!)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let layer = AVPlayerLayer(player: player)
+        
+        layer.frame = self.view.frame
+        layer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        
+        self.view.layer.addSublayer(layer)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    internal func play() {
+        player.play()
+    }
+    
+    internal func pause() {
+        player.pause()
+    }
+    
+}
